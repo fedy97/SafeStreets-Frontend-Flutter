011fbb3577fde8606beaf7b645ee4c6009fa73ed
evandro <maddesevandro@gmail.com>
2019-12-20 09:42:41 +0100

    -Added tests for violation_query_manager.dart -Update query results method in violation query manager

--- safe_streets/lib/services/violation_query_manager.dart
+++ safe_streets/lib/services/violation_query_manager.dart
@@ -3,18 +3,121 @@ import 'package:safe_streets/model/report/report_to_get.dart';
 import 'package:safe_streets/model/user/user.dart';
 
 class ViolationQueryManager {
-  static List<ReportToGet> queryResults(User u, String city,
-      Violation violation, DateTime timeFrom, DateTime timeTo) {
+  /**
+   * active: pos 0 city; pos 1 violation; pos 2 timeFrom; pos 3 timeTo
+   */
+  static List<ReportToGet> queryResults(
+      User u,
+      String city,
+      Violation violation,
+      DateTime timeFrom,
+      DateTime timeTo,
+      List<bool> active) {
     List<ReportToGet> results = new List<ReportToGet>();
     for (ReportToGet reportToGet in u.reportsGet) {
-      if (reportToGet.reportPosition.address
-              .toLowerCase()
-              .contains(city.toLowerCase()) &&
-          reportToGet.violation == violation &&
-          reportToGet.time.isBefore(timeTo) &&
-          reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
-    }
+      if (active[0] && active[1] && active[2] && active[3]) {
+        //Tutti i filtri attivi
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.violation == violation &&
+            reportToGet.time.isBefore(timeTo) &&
+            reportToGet.time.isAfter(timeFrom)) {
+          results.add(reportToGet);
+        }
+      }
+      if (active[0] && active[1] && active[2] && !active[3]) {
+        //non attivo timeTo
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.violation == violation &&
+            reportToGet.time.isBefore(timeFrom)) results.add(reportToGet);
+      }
+      if (active[0] && active[1] && !active[2] && !active[3]) {
+        //non attivo data
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.violation == violation) results.add(reportToGet);
+      }
+      if (active[0] && !active[1] && active[2] && active[3]) {
+        //filtro sulla violazione non attivo
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.time.isBefore(timeTo) &&
+            reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
+      }
+      if (active[0] && !active[1] && active[2] && !active[3]) {
+        // filtro sulle violazioni e sul timeTo non attivi
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.time.isBefore(timeFrom)) results.add(reportToGet);
+      }
+      if (active[0] && !active[1] && !active[2] && active[3]) {
+        // filtro sulle violazioni e sulla timeFrom non attivi
+        if (reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.time.isAfter(timeTo)) results.add(reportToGet);
+      }
+      if (active[0] && !active[1] && !active[2] && !active[3]) {
+        // filtro sulla violazione, su time From, su timeTo non attivi
+        if (reportToGet.reportPosition.address
+            .toLowerCase()
+            .contains(city.toLowerCase())) results.add(reportToGet);
+      }
+      if (!active[0] && active[1] && active[2] && active[3]) {
+        //filtro sulla posizione non attivo
+        if (reportToGet.violation == violation &&
+            reportToGet.time.isBefore(timeTo) &&
+            reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
+      }
+      if (!active[0] && active[1] && active[2] && !active[3]) {
+        //Filtri sulla posizione e su timeTo non attivi
+        if (reportToGet.violation == violation &&
+            reportToGet.time.isBefore(timeFrom)) results.add(reportToGet);
+      }
 
+      if (!active[0] && active[1] && !active[2] && active[3]) {
+        //Filtri sulla posizione e su timeFrom non attivi
+        if (reportToGet.violation == violation &&
+            reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
+      }
+
+      if (!active[0] && active[1] && !active[2] && !active[3]) {
+        //filtro sulla posizione sulle date non attivi
+        if (reportToGet.violation == violation) results.add(reportToGet);
+      }
+
+      if (!active[0] && !active[1] && active[2] && active[3]) {
+        //filtri su pozione e violazione non attivi
+        if (reportToGet.time.isBefore(timeTo) &&
+            reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
+      }
+
+      if (!active[0] && !active[1] && active[2] && !active[3]) {
+        //Filtri su posione, violazione, timeTo non attivi
+        if (reportToGet.time.isAfter(timeFrom)) results.add(reportToGet);
+      }
+
+      if (!active[0] && !active[1] && !active[2] && active[3]) {
+        //Tfiltri sulla posizione, violazion, timeFrom non attivi
+        if (reportToGet.time.isAfter(timeTo)) results.add(reportToGet);
+      }
+
+      if (!active[0] && !active[1] && !active[2] && !active[3]) {
+        //nessun filtro attivo
+        if ((reportToGet.reportPosition.address
+                .toLowerCase()
+                .contains(city.toLowerCase()) &&
+            reportToGet.violation == violation &&
+            reportToGet.time.isBefore(timeTo) &&
+            reportToGet.time.isAfter(timeFrom))) results = u.reportsGet;
+      }
+    }
     return results;
   }
 }
--- safe_streets/test/units_test/violation_query_manager_test.dart
+++ safe_streets/test/units_test/violation_query_manager_test.dart
@@ -4,9 +4,7 @@ import 'package:safe_streets/model/location.dart';
 import 'package:safe_streets/model/report/report_to_get.dart';
 import 'package:safe_streets/model/user/citizen.dart';
 import 'package:safe_streets/model/user/user.dart';
-import 'package:safe_streets/services/stats_manager.dart';
 import 'package:safe_streets/services/violation_query_manager.dart';
-import 'package:safe_streets/ui/violation_query.dart';
 
 void main() {
   //setup
@@ -16,10 +14,12 @@ void main() {
   ReportToGet report1 = new ReportToGet();
   ReportToGet report2 = new ReportToGet();
   ReportToGet report3 = new ReportToGet();
+  List<bool> bound = new List<bool>();
 
   test('One report respects the bounds of the query', () {
     //setup
     u.reportsGet.clear();
+    bound.clear();
     report0.reportPosition = new Location(45.4773, 9.1815);
     report0.reportPosition.address = "Milano";
     report0.time = DateTime.parse("2010-07-20 20:18:04Z");
@@ -28,101 +28,51 @@ void main() {
     String city = "Milano";
     DateTime timeFrom = DateTime.parse("2000-07-20 20:18:04Z");
     DateTime timeTo = data;
+    bound.add(true);
+    bound.add(true);
+    bound.add(true);
+    bound.add(true);
     //run
     List<ReportToGet> results = new List<ReportToGet>();
     results = ViolationQueryManager.queryResults(
-        u, city, Violation.double_parking, timeFrom, timeTo);
+        u, city, Violation.double_parking, timeFrom, timeTo, bound);
     final max = results.length;
     //verify
     expect(report0, results.first);
     expect(max, 1);
   });
 
-  test('Zero report respect the bounds of the query (violation bound)', () {
+  test('Two report respects the bounds of the query(no bounds on city)', () {
     //setup
     u.reportsGet.clear();
+    bound.clear();
     report0.reportPosition = new Location(45.4773, 9.1815);
     report0.reportPosition.address = "Milano";
     report0.time = DateTime.parse("2010-07-20 20:18:04Z");
-    report0.violation = Violation.parking_on_pavement;
-    u.reportsGet.add(report0);
-    String city = "Milano";
-    DateTime timeFrom = DateTime.parse("2000-07-20 20:18:04Z");
-    DateTime timeTo = data;
-    //run
-    List<ReportToGet> results = new List<ReportToGet>();
-    results = ViolationQueryManager.queryResults(
-        u, city, Violation.double_parking, timeFrom, timeTo);
-    final max = results.length;
-    //verify
-    expect(max, 0);
-  });
-
-  test('Zero report respect the bounds of the query (data time bound)', () {
-    //setup
-    u.reportsGet.clear();
-    report0.reportPosition = new Location(45.4773, 9.1815);
-    report0.reportPosition.address = "Milano";
-    report0.time = DateTime.parse("1999-07-20 20:18:04Z");
     report0.violation = Violation.double_parking;
     u.reportsGet.add(report0);
-    String city = "Milano";
-    DateTime timeFrom = DateTime.parse("2000-07-20 20:18:04Z");
-    DateTime timeTo = data;
-    //run
-    List<ReportToGet> results = new List<ReportToGet>();
-    results = ViolationQueryManager.queryResults(
-        u, city, Violation.double_parking, timeFrom, timeTo);
-    final max = results.length;
-    //verify
-    expect(max, 0);
-  });
-
-  test('Zero report respect the bounds of the query (city bound)', () {
-    //setup
-    u.reportsGet.clear();
-    report0.reportPosition = new Location(45.0781, 7.6761);
-    report0.reportPosition.address = "Torino";
-    report0.time = DateTime.parse("2010-07-20 20:18:04Z");
-    report0.violation = Violation.double_parking;
-    u.reportsGet.add(report0);
-    String city = "Milano";
-    DateTime timeFrom = DateTime.parse("2000-07-20 20:18:04Z");
-    DateTime timeTo = data;
-    //run
-    List<ReportToGet> results = new List<ReportToGet>();
-    results = ViolationQueryManager.queryResults(
-        u, city, Violation.double_parking, timeFrom, timeTo);
-    final max = results.length;
-    //verify
-    expect(max, 0);
-  });
-
-  test('Zero report respect the bounds of the query (city bound)', () {
-    //setup
-    u.reportsGet.clear();
-    report0.reportPosition = new Location(45.0781, 7.6761);
-    report0.reportPosition.address = "Milano";
-    report0.time = DateTime.parse("2002-07-20 20:18:04Z");
-    report0.violation = Violation.double_parking;
-  u.reportsGet.add(report0);
-
-    report1.reportPosition = new Location(45.0781, 7.6761);
-    report1.reportPosition.address = "Milano";
-    report1.time = DateTime.parse("2003-07-20 20:18:04Z");
+    report1.reportPosition = new Location(10.0, 11.0);
+    report1.reportPosition.address = "Torino";
+    report1.time = DateTime.parse("2005-07-20 20:18:04Z");
     report1.violation = Violation.double_parking;
-u.reportsGet.add(report1);
-
+    u.reportsGet.add(report1);
     String city = "Milano";
     DateTime timeFrom = DateTime.parse("2000-07-20 20:18:04Z");
     DateTime timeTo = data;
+    bound.add(false);//city
+    bound.add(true);//violation
+    bound.add(true);//timeFrom
+    bound.add(true);//timeTo
     //run
     List<ReportToGet> results = new List<ReportToGet>();
     results = ViolationQueryManager.queryResults(
-        u, city, Violation.double_parking, timeFrom, timeTo);
+        u, city, Violation.double_parking, timeFrom, timeTo, bound);
     final max = results.length;
     //verify
+    expect(report0, results.first);
+    expect(report1,results[1]);
     expect(max, 2);
   });
 
+  //todo coprire tutti gli if di query results
 }
